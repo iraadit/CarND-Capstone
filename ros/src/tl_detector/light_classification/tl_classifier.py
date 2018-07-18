5b999f970d70b8076af6b09ad63442c1f3e42037
nuges01 <segunoshin@gmail.com>
2018-07-18 12:25:48 -0400

    First attempt at classification.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,18 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        #load classifier
+        graph_path = 'light_classification/models/model_real/frozen_inference_graph.pb'
+        self.graph = self.load_graph(graph_path)
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+        self.boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+        self.scores = self.graph.get_tensor_by_name('detection_scores:0')
+        self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+        self.sess = tf.Session(graph=self.graph)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +24,40 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        #implement light color prediction
+        img_expand = np.expand_dims(image, axis=0)
+        (boxes, scores, classes, num_detections) = \
+            self.sess.run([self.boxes, self.scores, self.classes, self.num_detections], feed_dict={self.image_tensor:img_expand})
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
+        print('SCORES: ', scores[0])
+        print('CLASSES: ', classes[0])
+
+        if scores[0] > 0.5:
+            if classes[0] == 1:
+                print('GREEN')
+                return TrafficLight.GREEN
+            elif classes[0] == 2:
+                print('RED')
+                return TrafficLight.RED
+            elif classes[0] == 3:
+                print('YELLOW')
+                return TrafficLight.YELLOW
+
         return TrafficLight.UNKNOWN
+
+    def load_graph(frozen_graph_filename):
+        # We load the protobuf file from the disk and parse it to retrieve the
+        # unserialized graph_def
+        with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+
+        # Then, we import the graph_def into a new Graph and returns it
+        with tf.Graph().as_default() as graph:
+            # The name var will prefix every op/nodes in your graph
+            # Since we load everything in a new graph, this is not needed
+            tf.import_graph_def(graph_def, name="prefix")
+        return graph
